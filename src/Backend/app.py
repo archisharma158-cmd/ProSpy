2330b05f445f5a82c179ca2e1502793adb0b2ce8
archisharma158-cmd <archisharma158@gmail.com>
2026-06-29 22:03:05 +0530

    created required files

---
+++ src/Backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+model = joblib.load("prospy_model.joblib")
+
+@app.route("/")
+def home():
+    return "ProSpy API Running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    features = np.array(data["features"]).reshape(1, -1)
+
+    prediction = model.predict(features)
+
+    return jsonify({
+        "prediction": prediction.tolist()
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
